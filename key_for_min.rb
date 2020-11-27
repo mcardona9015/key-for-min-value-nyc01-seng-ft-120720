e0d21463bb6f426f9cb797161509e440b78c9693
Michael Cardona <mcardona9015@gmail.com>
2020-11-27 04:51:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,15 @@
 def key_for_min_value(name_hash)
   first_key, min_val = name_hash.first
   min_val_product = ''
-  name_hash.each do |product, price|
-    if price <= min_val
-      min_val = price
-      min_val_product = product
+  if name_hash.is_empty?
+    nil
+  else
+    name_hash.each do |product, price|
+      if price <= min_val
+        min_val = price
+        min_val_product = product
+      end
     end
+    min_val_product
   end
-  min_val_product
 end
