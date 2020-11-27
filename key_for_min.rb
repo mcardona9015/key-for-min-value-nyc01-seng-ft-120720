604bb6926d453857a8fb232c57e888ed576ccc57
Michael Cardona <mcardona9015@gmail.com>
2020-11-27 04:27:11 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_val = 0
+  min_val_product = ''
+  name_hash.each do |product, price|
+    if price < min_val
+      min_val = price
+      min_val_product = product
+    end
+  end
+  min_val_product
 end
